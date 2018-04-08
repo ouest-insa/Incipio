bbca8be4ae11ea938161de2afd0070084958f46a
stoakes <Stoakes@users.noreply.github.com>
2018-04-08 13:23:53 +0200

    Update dependencies
    Replace javereguiluz/easyadmin by easycorp/easyadmin

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -58,7 +58,7 @@ class AppKernel extends Kernel
             new Mgate\PersonneBundle\MgatePersonneBundle(),
             new Mgate\CommentBundle\MgateCommentBundle(),
             new Mgate\SuiviBundle\MgateSuiviBundle(),
-            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
+            new EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle(),
             new N7consulting\RhBundle\N7consultingRhBundle(),
             new N7consulting\PrivacyBundle\N7consultingPrivacyBundle(),
         ];
