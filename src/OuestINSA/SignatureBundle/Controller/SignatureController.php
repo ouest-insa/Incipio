dfc7d9cbcd6af12e6cf9eba41dbab9afafe62ff1
Pitou <pitou.games@gmail.com>
2018-12-13 16:40:18 +0100

    Clean signature bundle

--- src/OuestINSA/SignatureBundle/Controller/SignatureController.php
+++ src/OuestINSA/SignatureBundle/Controller/SignatureController.php
@@ -16,20 +16,13 @@ class SignatureController extends Controller
      */
     public function indexAction()
     {
-        //$entities = $this->getDoctrine()->getManager()->getRepository('OuestINSASignatureBundle:Signature')->findAll();
-        //$em = $this->getDoctrine()->getManager();
-        //$entities = $em->getRepository('MgateUserBundle:User')->findAll();
-        return $this->render('OuestINSASignatureBundle:Signature:index.html.twig', ['users' => $entities]);
-        //return $this->render('OuestINSASignatureBundle:Signature:index.html.twig');
-        //return new Response("It works !");
+        return $this->render('OuestINSASignatureBundle:Signature:index.html.twig');
     }
 
-
+    /**
+     * @Security("has_role('ROLE_SUIVEUR')")
+     */
     public function generateAction(){
       return $this->render('OuestINSASignatureBundle:Signature:generate.html.twig');
-    }
-
-    public function signatureTempAction(){
-      return $this->render('OuestINSASignatureBundle:Signature:temp.html.twig', ['users' => $entities]);
     }
 }
